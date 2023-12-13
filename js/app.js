f2291f0c3d6e33c7c922440c3df5e3f50e93717c
VladimirStega03 <114366736+VladimirStega03@users.noreply.github.com>
2023-12-13 13:18:41 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('mousemove', e => {
+	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+	const headerText = document.querySelector('.header__text');
+	const images = ['../img/1011.jpg', '../img/kislovodsk.jpg', '../img/47.jpg']; // Замените на пути к вашим изображениям
+	let currentIndex = 0;
+  
+	function changeBackground() {
+	  currentIndex = (currentIndex + 1) % images.length;
+	  headerText.style.backgroundImage = `url(${images[currentIndex]})`;
+	  setTimeout(changeBackground, 8000);
+	}
+  
+	setTimeout(changeBackground, 8000); // Начать смену изображений через 3 секунды
+  });
