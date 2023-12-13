5562a9ac14ebab47aba11e2d2569ed157dc796e2
VladimirStega03 <114366736+VladimirStega03@users.noreply.github.com>
2023-12-13 13:45:45 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ document.addEventListener('mousemove', e => {
 
 document.addEventListener('DOMContentLoaded', function () {
 	const headerText = document.querySelector('.header__text');
-	const images = ['../img/1011.jpg', '../img/kislovodsk.jpg', '../img/47.jpg']; // Замените на пути к вашим изображениям
+	const images = ['./img/1011.jpg', './img/kislovodsk.jpg', './img/47.jpg']; // Замените на пути к вашим изображениям
 	let currentIndex = 0;
   
 	function changeBackground() {
