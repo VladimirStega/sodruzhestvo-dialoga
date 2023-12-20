5554a58d7dfedbf4a2732b642765a15779a0db36
VladimirStega03 <114366736+VladimirStega03@users.noreply.github.com>
2023-12-20 17:19:08 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-document.addEventListener('mousemove', e => {
-	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
-})
+// document.addEventListener('mousemove', e => {
+// 	document.body.style.cssText = `--move-x: ${e.clientX}px; --move-y: ${e.clientY}px;`
+// })
 
 document.addEventListener('DOMContentLoaded', function () {
 	const headerText = document.querySelector('.header__text');
-	const images = ['./img/1011.jpg', './img/kislovodsk.jpg', './img/47.jpg']; // Замените на пути к вашим изображениям
+	const images = ['img/1011.jpg', 'img/1.jpg', 'img/0001.jpg', 'img/0002.jpg']; // Замените на пути к вашим изображениям
 	let currentIndex = 0;
   
 	function changeBackground() {
@@ -15,3 +15,12 @@ document.addEventListener('DOMContentLoaded', function () {
   
 	setTimeout(changeBackground, 8000); // Начать смену изображений через 3 секунды
   });
+
+
+  function downloadFile() {
+	var filename = "example.txt"; // Replace with the actual filename
+	var link = document.createElement('a');
+	link.href = "https://drive.google.com/file/d/1FWDZaj--QwQ3uLnVywuUHA0VHEjqDphQ/view?usp=drive_link"; // Replace with the actual link
+	link.download = filename;
+	link.click();
+  }
